7de49ac12fea44b254542d622b2596feecb9d568
Youssef Nafaa <nafaayoussef@gmail.com>
2022-02-09 01:31:13 -0500

    add ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,7 +14,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  res.json(urlDatabase);
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
 })
 
 app.listen(PORT, () => {
